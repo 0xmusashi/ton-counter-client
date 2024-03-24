8e32113f249d2f3b1948986c08582cea25d8496c
0xmusashi <musashihp42@gmail.com>
2024-03-24 22:33:51 +0700

    fix manifest url

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // this manifest is used temporarily for development purposes
-const manifestUrl = 'https://0xmusashi.github.ioton-counter-client/tonconnect-manifest.json';
+const manifestUrl = 'https://0xmusashi.github.io/ton-counter-client/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
